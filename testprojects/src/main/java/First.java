8f11c2bd6d7b786c3df2c1429f24fe9eca061476
Satha Sivam <satha.sivam@outlook.com>
2022-07-25 18:26:56 +0800

    Initial Commit

--- testprojects/src/main/java/First.java
+++ testprojects/src/main/java/First.java
@@ -1,6 +1,11 @@
+
 public class First {
 
     public static void main(String[] args) {
+        // Method
+        System.out.println("Hello World");
+        System.out.print("Hello World"); // syntax
+        
 
     }
 
