034576317fc0dd6d0fd7c909db125d268dc05262
Satha Sivam <satha.sivam@outlook.com>
2022-07-27 16:36:56 +0800

    Initial Commit

--- testprojects/src/main/java/First.java
+++ testprojects/src/main/java/First.java
@@ -1,11 +1,36 @@
 
-public class First {
+public class First { // case-sensitive
 
     public static void main(String[] args) {
         // Method
-        System.out.println("Hello World");
-        System.out.print("Hello World"); // syntax
-        
+        // System.out.println("Hello World");
+        // System.out.print("Hello World"); // syntax
+
+        // variables are typed in camelCased
+        int ticketPrice; //variable declaration -datatype variableName
+        ticketPrice = 900; // assigning value
+        boolean paid = true;
+        char c = 't';
+        System.out.println(paid);
+        System.out.println(c);
+        System.out.println("ticket price is " + ticketPrice);
+
+        System.out.println("Hello\nworld"); // escape sequences
+        System.out.println("\"Hello World\"");
+
+        int ticketPrice2 = 0XA; // Hexadecimal
+        long lng = 7L;
+        float pi = (float) 314159e-05;
+        float pi2 = (float) 3.14159;
+        int n = 0b01001101;
+        int n2 = 123_456_789; // underscored improves readability
+        System.out.println(pi);
+
+        byte b; // 1 byte
+        int n1 = 10; // 4 bytes
+        // n1 = b; // automatic type conversion
+        b = (byte) n1; // explicit declaration
+        System.out.println(b);
 
     }
 
