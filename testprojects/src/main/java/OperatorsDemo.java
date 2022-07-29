1aa2196de1b297bf585a5838d59f7c451fa8c961
Satha Sivam <satha.sivam@outlook.com>
2022-07-29 11:37:56 +0800

    Initial Commit

---
+++ testprojects/src/main/java/OperatorsDemo.java
@@ -0,0 +1,23 @@
+public class OperatorsDemo {
+
+    public static void main(String[] args) {
+//        boolean a =true, b=false;
+//
+//        System.out.println(a&b);
+
+        // Short Circuit Logical Operators
+//        int a=22;
+//
+//        System.out.println((a>20) && (a<30));
+//
+//        System.out.println((a<20) || (a>30));
+
+        // Operators Precedence
+        // BODMAS
+        int a=10, b=25;
+        double c = a*b-10/a;
+        System.out.println(c);
+
+    }
+
+}
