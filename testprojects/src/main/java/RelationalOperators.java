ef76efb7c01ddbc0e5ad20b8aebac97d8a1893dd
Satha Sivam <satha.sivam@outlook.com>
2022-07-29 11:36:30 +0800

    Initial Commit

--- testprojects/src/main/java/RelationalOperators.java
+++ testprojects/src/main/java/RelationalOperators.java
@@ -2,10 +2,32 @@ public class RelationalOperators {
 
     public static void main(String[] args) {
 
-        int a =10, b=10; // if
+//        int a =10, b=10; // if
+//
+//        System.out.println(a>b);
+//        System.out.println(a<b);
 
-        System.out.println(a>b);
-        System.out.println(a<b);
+        // Bitwise Operators
+//        int a=9, b=12;
+//
+//        int c = a & b;
+//        System.out.println("a&b is " + c);
+//        c = a | b;
+//        System.out.println("a|b is " +c);
+//        c = a ^ b;
+//        System.out.println("a^b is " + c);
+//        c = ~a;
+//        System.out.println("~a is " + c);
+
+        byte a=4;
+        System.out.println(a<<1); // promoted to int
+
+        int c =-5;
+
+        c = c&1;
+        c &=1;
+        System.out.println("Right shift " +(c>>1));
+        System.out.println("Right shift with zero fill " +(c>>>1));
 
     }
 
